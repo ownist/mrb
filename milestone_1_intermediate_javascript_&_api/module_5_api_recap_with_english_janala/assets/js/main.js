fdd101ac305395d236b6f0656257bf53d9e18495
codebyshahed <shahedevise@gmail.com>
2026-02-10 23:09:16 +0600

    feat: 5-3 Load and Display All Lessons with API as Figma

--- milestone_1_intermediate_javascript_&_api/module_5_api_recap_with_english_janala/assets/js/main.js
+++ milestone_1_intermediate_javascript_&_api/module_5_api_recap_with_english_janala/assets/js/main.js
@@ -1,1 +1,44 @@
-console.log("welcome to english janala");
+// load vocabularies category
+const loadVocabulariesCategory = async () => {
+  const url = "https://openapi.programming-hero.com/api/levels/all";
+
+  const res = await fetch(url);
+  const vocCategoriesData = await res.json();
+  displayVocCategories(vocCategoriesData.data);
+};
+
+const displayVocCategories = (lessons) => {
+  // get the container and empty the container
+  const vocabularyContainer = document.getElementById("voccabularyContainer");
+  vocabularyContainer.innerHTML = ""; // empty koira dichi
+
+  // loop trought
+  lessons.forEach((lesson) => {
+    // create an element
+    const btnDiv = document.createElement("div");
+    // set inside
+    btnDiv.innerHTML = `
+    <button class="flex items-center gap-1 text-sm font-thin px-2 sm:px-4 py-1 sm:py-2.5 rounded-sm border border-zinc-700 hover:bg-zinc-300">
+            <span>
+              <i class="ri-booklet-line"></i>
+            </span>
+            <p>Lesson - ${lesson.level_no}</p>
+          </button>
+    `;
+
+    // append into container
+    vocabularyContainer.appendChild(btnDiv);
+  });
+
+  /**
+   * data structure
+   * 
+   * {
+    "id": 101,
+    "level_no": 1,
+    "lessonName": "Basic Vocabulary"
+    }
+   */
+};
+
+loadVocabulariesCategory();
