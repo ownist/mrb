fea13c6cf4ab7da80415cb4a1ddc93be4bce56df
ownist <shahedevise@gmail.com>
2026-02-20 00:12:07 +0600

    feat: 7-4 Array and object Destructuring

---
+++ milestone_2_introduction_to_simple_react/module_7_javascript_you_need_to_know_for_react/destructuring.js
@@ -0,0 +1,16 @@
+// const friends = ["shahed", "sabbir", "niloy", "siam"];
+// const [a, b, c, d] = friends;
+// console.log(b);
+// console.log(a);
+// console.log(d);
+
+const person = {
+  name: "hero alam",
+  age: 27,
+  friends: ["shahed", "siam"],
+  country: "bangladesh",
+};
+
+const { name, age, friends, country } = person;
+
+console.log(country);
