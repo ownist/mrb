0370ce9152b799b67b6ff40c57e4f2f7180e9fec
ownist <shahedevise@gmail.com>
2026-02-21 00:17:42 +0600

    feat: 7-8 dot vs bracket notation

---
+++ milestone_2_introduction_to_simple_react/module_7_javascript_you_need_to_know_for_react/notation.js
@@ -0,0 +1,13 @@
+const person = {
+  name: "hero alam",
+  age: 10,
+  friends: ["karim", "rahim", "salim"],
+  salary: 10000,
+  10: "secrate code",
+};
+
+const heroName = person["name"];
+console.log(heroName);
+
+const secrateCode = person[10];
+console.log(secrateCode);
